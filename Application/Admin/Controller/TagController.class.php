3e3f2106174f5c7f1e2704f840feb8b4a1aa144e
findever <ifindever@gmail.com>
2015-06-03 18:40:52 +0800

    缩略图

--- Application/Admin/Controller/TagController.class.php
+++ Application/Admin/Controller/TagController.class.php
@@ -4,25 +4,25 @@
  * @author Findever 
  * @link ifindever.com 
  * @datetime 2015-6-2 20:04:24
- * @Description 文章分类基类
+ * @Description 标签分类基类
  */
 
 namespace Admin\Controller;
 
 use Admin\Controller\BaseController;
 
-class CategoryController extends BaseController {
+class TagController extends BaseController {
 
 	public function index() {
 		
 	}
 
 	/**
-	 * 获取分类列表，ajax返回json数据
+	 * 获取标签列表，ajax返回json数据
 	 * @access public
 	 */
 	public function ajaxList() {
-		$data = M('category')->select();
+		$data = M('tag')->select();
 		$this->ajaxReturn($data);
 	}
 
