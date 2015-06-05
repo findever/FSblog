152a652b8f0b63a8d2d36ef31e018141f020e3e5
findever <ifindever@gmail.com>
2015-06-05 17:47:02 +0800

    编辑细节

--- Application/Admin/Model/PostModel.class.php
+++ Application/Admin/Model/PostModel.class.php
@@ -14,7 +14,6 @@ use Think\Model;
 class PostModel extends Model {
 
 	protected $_validate = array(
-		array('id', 'require', '验证码必须！'), //默认情况下用正则进行验证
 		array('post_author', '', '帐号名称已经存在！', 0, 'unique', 1), // 在新增的时候验证name字段是否唯一
 		array('post_date', array(1, 2, 3), '值的范围不正确！', 2, 'in'), // 当值不为空的时候判断是否在一个范围内
 		array('post_title', 'password', '确认密码不正确', 0, 'confirm'), // 验证确认密码是否和密码一致
