fe35f6e71863ed33927633f46f64adbbd50e294d
findever <ifindever@gmail.com>
2015-06-16 22:24:11 +0800

    分类管理

--- Application/Admin/Controller/CategoryController.class.php
+++ Application/Admin/Controller/CategoryController.class.php
@@ -14,7 +14,38 @@ use Admin\Controller\BaseController;
 class CategoryController extends BaseController {
 
 	public function index() {
-		
+		$cates = M('category')->where("id>=" . \Admin\Model\CategoryModel::$otherID)->select();
+		$this->assign("cates", $cates);
+		$this->display();
+	}
+
+	/**
+	 * ajax快速添加分类
+	 * @access public
+	 */
+	public function ajaxAdd() {
+		$cateModel = D('category');
+
+		$result = array(
+			"status" => 0,
+			"msg" => "添加成功！"
+		);
+
+		if (!$cateModel->create()) {
+			$result = array(
+				"status" => 1,
+				"msg" => $cateModel->getError()
+			);
+		} else {
+			if (!$cateModel->add()) {
+				$result = array(
+					"status" => 2,
+					"msg" => "添加失败".$cateModel->getDbError()
+				);
+			}
+		}
+
+		$this->ajaxReturn($result);
 	}
 
 	/**
--- Application/Admin/Model/CategoryModel.class.php
+++ Application/Admin/Model/CategoryModel.class.php
@@ -16,5 +16,15 @@ class CategoryModel extends Model {
 	static $recycleID = 1;
 	static $draftID = 2;
 	static $otherID = 3;
+	protected $_validate = array(
+		array('id', 'number', '非法更新！', self::VALUE_VALIDATE),
+		array('cate_name', 'require', '分类名称不能为空', self::MUST_VALIDATE),
+		array('cate_name', '', '分类名称已存在', self::MUST_VALIDATE, "unique"),
+		array('cate_name', '1,10', '分类名称长度不能超过10个字符', self::MUST_VALIDATE, 'length')
+	);
+	protected $_auto = array(
+		array('is_system', 0, self::MODEL_BOTH, 'string'),
+		array('parent_id', 0, self::MODEL_BOTH, 'string')
+	);
 
 }
